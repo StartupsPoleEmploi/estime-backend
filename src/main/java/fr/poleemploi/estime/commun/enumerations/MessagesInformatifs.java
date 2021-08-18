7bc35eb2159b5f60c4088ce1044f9600a946aa29
Lucas Détré <lucas.detre@gmail.com>
2021-08-18 13:55:09 +0200

    us 150

--- src/main/java/fr/poleemploi/estime/commun/enumerations/MessagesInformatifs.java
+++ src/main/java/fr/poleemploi/estime/commun/enumerations/MessagesInformatifs.java
@@ -1,7 +1,8 @@
 package fr.poleemploi.estime.commun.enumerations;
 
 public enum MessagesInformatifs {
-
+    
+    AGEPI_IDF("Si vous êtes domicilé⸱e en Ile de France, veuillez vous rapprocher de votre conseiller référent pour en faire la demande. (Les montants peuvent être légèrement diférents)."),
     ASS_DEMANDE_RENOUVELLEMENT("Le droit à l’ASS est calculé pour 6 mois et il peut être renouvelé en fonction de vos ressources. Vous allez recevoir un dossier de demande de renouvellement à compléter.");
     
     private String message;
