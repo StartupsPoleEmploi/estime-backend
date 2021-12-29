6eeb83b93e24b4f567945397f78c35976d17b706
lanceleurj@gmail.com <jeremie.lanceleur@pole-emploi.fr>
2021-12-29 09:22:51 +0100

    refacto

--- src/test/java/testsintegration/simulation/temporalite/are/Commun.java
+++ src/test/java/testsintegration/simulation/temporalite/are/Commun.java
@@ -9,7 +9,6 @@ import fr.poleemploi.estime.clientsexternes.poleemploiio.PoleEmploiIOClient;
 import fr.poleemploi.estime.commun.enumerations.NationaliteEnum;
 import fr.poleemploi.estime.commun.enumerations.TypeContratTravailEnum;
 import fr.poleemploi.estime.commun.enumerations.TypePopulationEnum;
-import fr.poleemploi.estime.commun.utile.BearerTokenUtile;
 import fr.poleemploi.estime.commun.utile.DateUtile;
 import fr.poleemploi.estime.commun.utile.SuiviUtilisateurUtile;
 import fr.poleemploi.estime.services.ressources.AllocationARE;
@@ -19,53 +18,50 @@ import utile.tests.Utile;
 
 public class Commun {
 
-    @Autowired
-    protected Utile utile;
+	@Autowired
+	protected Utile utile;
 
-    @SpyBean
-    protected DateUtile dateUtile;
+	@SpyBean
+	protected DateUtile dateUtile;
 
-    @SpyBean
-    private SuiviUtilisateurUtile suiviUtilisateurUtile;
+	@SpyBean
+	private SuiviUtilisateurUtile suiviUtilisateurUtile;
 
-    @SpyBean
-    private PoleEmploiIOClient poleEmploiIOClient;
+	@SpyBean
+	private PoleEmploiIOClient poleEmploiIOClient;
 
-    @SpyBean
-    private BearerTokenUtile accessTokenUtile;
+	protected DemandeurEmploi createDemandeurEmploi(int prochaineDeclarationTrimestrielle) throws ParseException {
+		boolean isEnCouple = false;
+		int nbEnfant = 1;
+		DemandeurEmploi demandeurEmploi = utile.creerBaseDemandeurEmploi(TypePopulationEnum.ARE.getLibelle(), isEnCouple, nbEnfant);
 
-    protected DemandeurEmploi createDemandeurEmploi(int prochaineDeclarationTrimestrielle) throws ParseException {
-	boolean isEnCouple = false;
-	int nbEnfant = 1;
-	DemandeurEmploi demandeurEmploi = utile.creerBaseDemandeurEmploi(TypePopulationEnum.ARE.getLibelle(), isEnCouple, nbEnfant);
+		demandeurEmploi.getInformationsPersonnelles().setDateNaissance(utile.getDate("05-07-1986"));
+		demandeurEmploi.getInformationsPersonnelles().setNationalite(NationaliteEnum.FRANCAISE.getValeur());
+		demandeurEmploi.getInformationsPersonnelles().setCodePostal("44200");
 
-	demandeurEmploi.getInformationsPersonnelles().setDateNaissance(utile.getDate("05-07-1986"));
-	demandeurEmploi.getInformationsPersonnelles().setNationalite(NationaliteEnum.FRANCAISE.getValeur());
-	demandeurEmploi.getInformationsPersonnelles().setCodePostal("44200");
+		demandeurEmploi.getSituationFamiliale().getPersonnesACharge().get(0).getInformationsPersonnelles().setDateNaissance(utile.getDateNaissanceFromAge(9));
 
-	demandeurEmploi.getSituationFamiliale().getPersonnesACharge().get(0).getInformationsPersonnelles().setDateNaissance(utile.getDateNaissanceFromAge(9));
+		demandeurEmploi.getFuturTravail().setTypeContrat(TypeContratTravailEnum.CDI.name());
+		demandeurEmploi.getFuturTravail().setNombreHeuresTravailleesSemaine(35);
+		demandeurEmploi.getFuturTravail().getSalaire().setMontantNet(940);
+		demandeurEmploi.getFuturTravail().getSalaire().setMontantBrut(1200);
+		demandeurEmploi.getFuturTravail().setDistanceKmDomicileTravail(80);
+		demandeurEmploi.getFuturTravail().setNombreTrajetsDomicileTravail(12);
+		Salaire salaire = new Salaire();
+		salaire.setMontantBrut(350);
+		salaire.setMontantNet(200);
+		demandeurEmploi.getRessourcesFinancieres().setSalaire(salaire);
 
-	demandeurEmploi.getFuturTravail().setTypeContrat(TypeContratTravailEnum.CDI.name());
-	demandeurEmploi.getFuturTravail().setNombreHeuresTravailleesSemaine(35);
-	demandeurEmploi.getFuturTravail().getSalaire().setMontantNet(940);
-	demandeurEmploi.getFuturTravail().getSalaire().setMontantBrut(1200);
-	demandeurEmploi.getFuturTravail().setDistanceKmDomicileTravail(80);
-	demandeurEmploi.getFuturTravail().setNombreTrajetsDomicileTravail(12);
-	Salaire salaire = new Salaire();
-	salaire.setMontantBrut(350);
-	salaire.setMontantNet(200);
-	demandeurEmploi.getRessourcesFinancieres().setSalaire(salaire);
+		AllocationARE allocationARE = new AllocationARE();
+		allocationARE.setAllocationJournaliereNet(15f);
+		allocationARE.setAllocationMensuelleNet(150f);
+		allocationARE.setConcerneDegressivite(false);
+		allocationARE.setNombreJoursRestants(15f);
+		allocationARE.setMontantJournalierBrut(27f);
+		allocationARE.setSalaireJournalierReferenceBrut(45f);
+		demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().setAllocationARE(allocationARE);
 
-	AllocationARE allocationARE = new AllocationARE();
-	allocationARE.setAllocationJournaliereNet(15f);
-	allocationARE.setAllocationMensuelleNet(150f);
-	allocationARE.setConcerneDegressivite(false);
-	allocationARE.setNombreJoursRestants(15f);
-	allocationARE.setMontantJournalierBrut(27f);
-	allocationARE.setSalaireJournalierReferenceBrut(45f);
-	demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().setAllocationARE(allocationARE);
-
-	return demandeurEmploi;
-    }
+		return demandeurEmploi;
+	}
 
 }
