c761e576adf9196b8c03080ab8afc88bd1aa8206
Lucas Détré <lucas.detre@gmail.com>
2022-01-06 09:30:46 +0100

    return ArePEIOIN plutôt qu'un string

--- src/main/java/fr/poleemploi/estime/clientsexternes/poleemploiio/utile/SimulateurRepriseActiviteUtile.java
+++ src/main/java/fr/poleemploi/estime/clientsexternes/poleemploiio/utile/SimulateurRepriseActiviteUtile.java
@@ -11,22 +11,22 @@ import fr.poleemploi.estime.services.ressources.DemandeurEmploi;
 @Component
 public class SimulateurRepriseActiviteUtile {
 
-    public HttpEntity<String> createHttpEntityAre(DemandeurEmploi demandeurEmploi) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", demandeurEmploi.getPeConnectAuthorization().getBearerToken());
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return new HttpEntity<String>(createArePEIOIn(demandeurEmploi), headers);
+    public HttpEntity<ArePEIOIn> createHttpEntityAre(DemandeurEmploi demandeurEmploi) {
+	HttpHeaders headers = new HttpHeaders();
+	headers.add("Authorization", demandeurEmploi.getPeConnectAuthorization().getBearerToken());
+	headers.setContentType(MediaType.APPLICATION_JSON);
+	return new HttpEntity<ArePEIOIn>(createArePEIOIn(demandeurEmploi), headers);
     }
-    
-    private String createArePEIOIn(DemandeurEmploi demandeurEmploi) {
-        ArePEIOIn areIn = new ArePEIOIn();
-        //TODO JLA contrôle à déporter dans controleur couche service
-        if (demandeurEmploi.getRessourcesFinancieres() != null && demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi() != null
-                && demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().getAllocationARE() != null) {
-            areIn.setAllocationBruteJournaliere(demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().getAllocationARE().getMontantJournalierBrut());
-            areIn.setSalaireBrutJournalier(demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().getAllocationARE().getSalaireJournalierReferenceBrut());
-            areIn.setGainBrut(demandeurEmploi.getFuturTravail().getSalaire().getMontantBrut());
-        } 
-        return areIn.toString();
+
+    private ArePEIOIn createArePEIOIn(DemandeurEmploi demandeurEmploi) {
+	ArePEIOIn areIn = new ArePEIOIn();
+	//TODO JLA contrôle à déporter dans controleur couche service
+	if (demandeurEmploi.getRessourcesFinancieres() != null && demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi() != null
+		&& demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().getAllocationARE() != null) {
+	    areIn.setAllocationBruteJournaliere(demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().getAllocationARE().getMontantJournalierBrut());
+	    areIn.setSalaireBrutJournalier(demandeurEmploi.getRessourcesFinancieres().getAidesPoleEmploi().getAllocationARE().getSalaireJournalierReferenceBrut());
+	    areIn.setGainBrut(demandeurEmploi.getFuturTravail().getSalaire().getMontantBrut());
+	}
+	return areIn;
     }
 }
