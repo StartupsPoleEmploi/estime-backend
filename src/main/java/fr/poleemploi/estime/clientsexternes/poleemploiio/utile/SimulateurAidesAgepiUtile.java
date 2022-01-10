97b6718e164bff1a50ba7a5c4197c072d5d0c22a
Lucas Détré <lucas.detre@gmail.com>
2022-01-10 16:33:56 +0100

    fix agepi / aide mob

--- src/main/java/fr/poleemploi/estime/clientsexternes/poleemploiio/utile/SimulateurAidesAgepiUtile.java
+++ src/main/java/fr/poleemploi/estime/clientsexternes/poleemploiio/utile/SimulateurAidesAgepiUtile.java
@@ -1,8 +1,5 @@
 package fr.poleemploi.estime.clientsexternes.poleemploiio.utile;
 
-import java.time.LocalDate;
-import java.time.Period;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -11,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 import fr.poleemploi.estime.clientsexternes.poleemploiio.ressources.simulateuraides.agepi.AgepiPEIOIn;
 import fr.poleemploi.estime.commun.utile.DateUtile;
+import fr.poleemploi.estime.commun.utile.demandeuremploi.SituationFamilialeUtile;
 import fr.poleemploi.estime.services.ressources.DemandeurEmploi;
-import fr.poleemploi.estime.services.ressources.Personne;
 
 @Component
 public class SimulateurAidesAgepiUtile {
@@ -20,46 +17,35 @@ public class SimulateurAidesAgepiUtile {
     @Autowired
     private DateUtile dateUtile;
 
+    @Autowired
+    private SituationFamilialeUtile situationFamilialeUtile;
+
     private static final String CONTEXTE_REPRISE_EMPLOI = "reprise";
 
     public HttpEntity<AgepiPEIOIn> createHttpEntityAgepiSimulateurAides(DemandeurEmploi demandeurEmploi) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", demandeurEmploi.getPeConnectAuthorization().getBearerToken());
-        AgepiPEIOIn agepiPEIOIn = createAgepiPEIOIn(demandeurEmploi);
-        return new HttpEntity<AgepiPEIOIn>(agepiPEIOIn, headers);
+	HttpHeaders headers = new HttpHeaders();
+	headers.setContentType(MediaType.APPLICATION_JSON);
+	headers.add("Authorization", demandeurEmploi.getPeConnectAuthorization().getBearerToken());
+	AgepiPEIOIn agepiPEIOIn = createAgepiPEIOIn(demandeurEmploi);
+	return new HttpEntity<AgepiPEIOIn>(agepiPEIOIn, headers);
     }
 
     private AgepiPEIOIn createAgepiPEIOIn(DemandeurEmploi demandeurEmploi) {
-        AgepiPEIOIn agepiPEIOIn = new AgepiPEIOIn();
-        agepiPEIOIn.setCodeTerritoire("001");
-        agepiPEIOIn.setContexte(CONTEXTE_REPRISE_EMPLOI);
-        agepiPEIOIn.setDateActionReclassement(dateUtile.getDateJour().toString());
-        agepiPEIOIn.setDateDepot(dateUtile.getDateJour().toString());
-        agepiPEIOIn.setDureePeriodeEmploiOuFormation(demandeurEmploi.getRessourcesFinancieres().getNombreMoisTravaillesDerniersMois());
-        agepiPEIOIn.setEleveSeulEnfants(!demandeurEmploi.getSituationFamiliale().getIsEnCouple());
-        agepiPEIOIn.setIntensite(Math.round(demandeurEmploi.getFuturTravail().getNombreHeuresTravailleesSemaine()));
-        agepiPEIOIn.setLieuFormationOuEmploi("France");
-        agepiPEIOIn.setNatureContratTravail(demandeurEmploi.getFuturTravail().getTypeContrat());
+	AgepiPEIOIn agepiPEIOIn = new AgepiPEIOIn();
+	agepiPEIOIn.setContexte(CONTEXTE_REPRISE_EMPLOI);
+	agepiPEIOIn.setOrigine("W");
+	agepiPEIOIn.setCodeTerritoire("001");
+	agepiPEIOIn.setDateActionReclassement(dateUtile.getDateJour().toString());
+	agepiPEIOIn.setDateDepot(dateUtile.getDateJour().toString());
+	agepiPEIOIn.setNatureContratTravail(demandeurEmploi.getFuturTravail().getTypeContrat());
+	agepiPEIOIn.setIntensite(Math.round(demandeurEmploi.getFuturTravail().getNombreHeuresTravailleesSemaine()));
+	agepiPEIOIn.setTypeIntensite("Hebdomadaire");
+	agepiPEIOIn.setDureePeriodeEmploiOuFormation(demandeurEmploi.getRessourcesFinancieres().getNombreMoisTravaillesDerniersMois());
+	agepiPEIOIn.setLieuFormationOuEmploi("France");
+	agepiPEIOIn.setEleveSeulEnfants(!situationFamilialeUtile.isEnCouple(demandeurEmploi));
+	agepiPEIOIn.setNombreEnfants(situationFamilialeUtile.getNombrePersonnesAChargeAgeInferieureAgeLimite(demandeurEmploi, 21));
+	agepiPEIOIn.setNombreEnfantsMoins10Ans(situationFamilialeUtile.getNombrePersonnesAChargeAgeInferieureAgeLimite(demandeurEmploi, 10));
 
-        //@TODO JLA à extraire
-        int nombreEnfants = 0;
-        int nombreEnfantsMoinsDixAns = 0;
-        for (Personne personneACharge : demandeurEmploi.getSituationFamiliale().getPersonnesACharge()) {
-            LocalDate today = LocalDate.now();
-            int agePersonneACharge = Period.between(personneACharge.getInformationsPersonnelles().getDateNaissance(), today).getYears();
-            if (agePersonneACharge < 10) {
-                ++nombreEnfants;
-                ++nombreEnfantsMoinsDixAns;
-            } else if (agePersonneACharge < 18) {
-                ++nombreEnfants;
-            }
-        }
-
-        agepiPEIOIn.setNombreEnfants(nombreEnfants);
-        agepiPEIOIn.setNombreEnfantsMoins10Ans(nombreEnfantsMoinsDixAns);
-        agepiPEIOIn.setOrigine("c");
-        agepiPEIOIn.setTypeIntensite("mensuelle");
-        return agepiPEIOIn;
+	return agepiPEIOIn;
     }
 }
