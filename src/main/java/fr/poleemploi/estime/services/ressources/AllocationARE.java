1feae9313a6f16122055789aa4ad625e1acada64
Lucas Détré <lucas.detre@gmail.com>
2022-04-29 16:49:59 +0200

    US69 : dégressivité ARE

--- src/main/java/fr/poleemploi/estime/services/ressources/AllocationARE.java
+++ src/main/java/fr/poleemploi/estime/services/ressources/AllocationARE.java
@@ -1,11 +1,17 @@
 package fr.poleemploi.estime.services.ressources;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AllocationARE {
 
     private Float allocationJournaliereBrute;
     private Float allocationMensuelleNet;
     private Float salaireJournalierReferenceBrut;
     private Float nombreJoursRestants;
+    @JsonProperty("hasDegressiviteAre")
+    private boolean hasDegressiviteAre;
+    private boolean isTauxPlein;
+    private boolean isTauxReduit;
 
     public Float getAllocationMensuelleNet() {
 	return allocationMensuelleNet;
@@ -39,9 +45,35 @@ public class AllocationARE {
 	this.nombreJoursRestants = nombreJoursRestants;
     }
 
+    public boolean hasDegressiviteAre() {
+	return hasDegressiviteAre;
+    }
+
+    @JsonProperty("hasDegressiviteAre")
+    public void setHasDegressiviteAre(boolean hasDegressiviteAre) {
+	this.hasDegressiviteAre = hasDegressiviteAre;
+    }
+
+    public boolean isTauxPlein() {
+	return isTauxPlein;
+    }
+
+    public void setTauxPlein(boolean isTauxPlein) {
+	this.isTauxPlein = isTauxPlein;
+    }
+
+    public boolean isTauxReduit() {
+	return isTauxReduit;
+    }
+
+    public void setTauxReduit(boolean isTauxReduit) {
+	this.isTauxReduit = isTauxReduit;
+    }
+
     @Override
     public String toString() {
 	return "AllocationARE [allocationJournaliereBrute=" + allocationJournaliereBrute + ", allocationMensuelleNet=" + allocationMensuelleNet
-		+ ", salaireJournalierReferenceBrut=" + salaireJournalierReferenceBrut + ", nombreJoursRestants=" + nombreJoursRestants + "]";
+		+ ", salaireJournalierReferenceBrut=" + salaireJournalierReferenceBrut + ", nombreJoursRestants=" + nombreJoursRestants + ", hasDegressiviteAre="
+		+ hasDegressiviteAre + ", isTauxPlein=" + isTauxPlein + ", isTauxReduit=" + isTauxReduit + "]";
     }
 }
