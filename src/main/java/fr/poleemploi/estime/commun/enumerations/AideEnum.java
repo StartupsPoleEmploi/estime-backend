014e8c6caa34916fbc9a2957e0cdd977193e95f8
Lucas Détré <lucas.detre@gmail.com>
2022-09-14 22:37:19 +0200

    US195 : ajout d'un texte d'aide pour les aides au logement

--- src/main/java/fr/poleemploi/estime/commun/enumerations/AideEnum.java
+++ src/main/java/fr/poleemploi/estime/commun/enumerations/AideEnum.java
@@ -3,9 +3,11 @@ package fr.poleemploi.estime.commun.enumerations;
 public enum AideEnum {
 
     AGEPI("AGEPI", "Aide à la Garde d'Enfants pour Parent Isolé", "agepi.html"), AIDE_MOBILITE("AM", "Aide à la mobilité", "aide-mobilite.html"),
-    AIDE_PERSONNALISEE_LOGEMENT("APL", "Aide Personnalisée au Logement", ""), AIDE_RETOUR_EMPLOI("ARE", "Allocation de Retour à l'Emploi", ""),
-    COMPLEMENT_AIDE_RETOUR_EMPLOI("ARE", "Complément d'Allocation de Retour à l'Emploi", ""), ALLOCATION_ADULTES_HANDICAPES("AAH", "Allocation aux Adultes Handicapés", "aah.html"),
-    ALLOCATION_LOGEMENT_FAMILIALE("ALF", "Allocation de Logement Familiale", ""), ALLOCATION_LOGEMENT_SOCIALE("ALS", "Allocation de Logement Sociale", ""),
+    AIDES_LOGEMENT("AL", "Aides au Logement", "aide_logement.html"), AIDE_PERSONNALISEE_LOGEMENT("APL", "Aide Personnalisée au Logement", "aide_logement.html"),
+    AIDE_RETOUR_EMPLOI("ARE", "Allocation de Retour à l'Emploi", ""), COMPLEMENT_AIDE_RETOUR_EMPLOI("ARE", "Complément d'Allocation de Retour à l'Emploi", ""),
+    ALLOCATION_ADULTES_HANDICAPES("AAH", "Allocation aux Adultes Handicapés", "aah.html"),
+    ALLOCATION_LOGEMENT_FAMILIALE("ALF", "Allocation de Logement Familiale", "aide_logement.html"),
+    ALLOCATION_LOGEMENT_SOCIALE("ALS", "Allocation de Logement Sociale", "aide_logement.html"),
     ALLOCATION_SOLIDARITE_SPECIFIQUE("ASS", "Allocation de Solidarité Spécifique", "ass.html"), ALLOCATIONS_FAMILIALES("AF", "Allocations Familiales", ""),
     ALLOCATION_SOUTIEN_FAMILIAL("ASF", "Allocation de Soutien Familial", ""), COMPLEMENT_FAMILIAL("CF", "Complément Familial", ""), IMMOBILIER("IMMO", "Revenus immobiliers", ""),
     MICRO_ENTREPRENEUR("MICRO", "Revenus micro-entreprise", ""), PRIME_ACTIVITE("PA", "Prime d'Activité", "prime-activite.html"),
