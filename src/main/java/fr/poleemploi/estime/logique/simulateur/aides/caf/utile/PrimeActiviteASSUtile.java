70e97e2e788e23c137de08d25304228681987a65
Lucas Détré <lucas.detre@gmail.com>
2022-09-23 21:28:52 +0200

    US190 : Anomalie temporalité PPA pour bénéficiaires ASS

--- src/main/java/fr/poleemploi/estime/logique/simulateur/aides/caf/utile/PrimeActiviteASSUtile.java
+++ src/main/java/fr/poleemploi/estime/logique/simulateur/aides/caf/utile/PrimeActiviteASSUtile.java
@@ -22,24 +22,24 @@ public class PrimeActiviteASSUtile {
      * @param numeroMoisSimule
      * @param prochaineDeclarationTrimestrielle
      * @return
-     *       _________________________________________________________________________________________________________
-     *      |                          |          |          |          |          |          |           |          |
-     *      | Mois cumul salaire / ass |    M0    |    M1    |    M2    |    M3    |    M4    |     M5    |    M6    |
-     *      |                          |          |          |          |          |          |           |          |
-     *      |  -        0              |          |          |          |          |   (C1)   |     V1    |    R1    |          
-     *      |  -        1              |          |          |          |   (C1)   |    V1    |     R1    | (C2)/R1  |               
-     *      |  -        2              |          |          |   (C1)   |    V1    |    R1    |  (C2)/R1  |    V2    |          
-     *      |  -        3              |          |   (C1)   |    V1    |    R1    | (C2)/R1  |     V2    |    R2    |
-     *      |__________________________|__________|__________|__________|__________|__________|___________|__________|
+     *       _______________________________________________________________________________________________________
+     *      |                          |          |          |          |          |          |          |          |
+     *      | Mois cumul salaire / ass |    M0    |    M1    |    M2    |    M3    |    M4    |    M5    |    M6    |
+     *      |                          |          |          |          |          |          |          |          |
+     *      |  -        0              |          |          |          |   (C1)   |    V1    |    R1    |    R1    |          
+     *      |  -        1              |          |          |   (C1)   |    V1    |    R1    | R1/(C2)  |    V2    |               
+     *      |  -        2              |          |   (C1)   |    V1    |    R1    | R1/(C2)  |    V2    |    R2    |          
+     *      |  -        3              |   (C1)   |    V1    |    R1    |  R1/C2   |    V2    |    R2    |    R2    |
+     *      |__________________________|__________|__________|__________|__________|__________|__________|__________|
      */
     public boolean isPrimeActiviteACalculer(int numeroMoisSimule, DemandeurEmploi demandeurEmploi) {
 	if (ressourcesFinancieresUtile.hasTravailleAuCoursDerniersMoisAvantSimulation(demandeurEmploi)) {
 	    int nombreMoisTravaillesDerniersMois = periodeTravailleeAvantSimulationUtile.getNombreMoisTravaillesAuCoursDes3DerniersMoisAvantSimulation(demandeurEmploi);
-	    return nombreMoisTravaillesDerniersMois == 1 && (numeroMoisSimule == 3 || numeroMoisSimule == 6)
-		    || (nombreMoisTravaillesDerniersMois == 2 && (numeroMoisSimule == 2 || numeroMoisSimule == 5))
-		    || (nombreMoisTravaillesDerniersMois == 3 && (numeroMoisSimule == 1 || numeroMoisSimule == 4));
+	    return nombreMoisTravaillesDerniersMois == 1 && (numeroMoisSimule == 2 || numeroMoisSimule == 5)
+		    || (nombreMoisTravaillesDerniersMois == 2 && (numeroMoisSimule == 1 || numeroMoisSimule == 4))
+		    || (nombreMoisTravaillesDerniersMois == 3 && (numeroMoisSimule == 0 || numeroMoisSimule == 3));
 	} else
-	    return numeroMoisSimule == 4;
+	    return numeroMoisSimule == 3;
     }
 
     /**
@@ -48,22 +48,23 @@ public class PrimeActiviteASSUtile {
      * @param numeroMoisSimule
      * @param prochaineDeclarationTrimestrielle
      * @return
-     *       _________________________________________________________________________________________________________
-     *      |                          |          |          |          |          |          |           |          |
-     *      | Mois cumul salaire / ass |    M0    |    M1    |    M2    |    M3    |    M4    |     M5    |    M6    |
-     *      |                          |          |          |          |          |          |           |          |
-     *      |  -        0              |          |          |          |          |    C1    |    (V1)   |    R1    |          
-     *      |  -        1              |          |          |          |    C1    |   (V1)   |     R1    |  C2/R1   |               
-     *      |  -        2              |          |          |    C1    |   (V1)   |    R1    |   C2/R1   |   (V2)   |          
-     *      |  -        3              |          |    C1    |   (V1)   |    R1    |  C2/R1   |    (V2)   |    R2    |
-     *      |__________________________|__________|__________|__________|__________|__________|___________|__________|
+     *       _______________________________________________________________________________________________________
+     *      |                          |          |          |          |          |          |          |          |
+     *      | Mois cumul salaire / ass |    M0    |    M1    |    M2    |    M3    |    M4    |    M5    |    M6    |
+     *      |                          |          |          |          |          |          |          |          |
+     *      |  -        0              |          |          |          |    C1    |   (V1)   |    R1    |    R1    |          
+     *      |  -        1              |          |          |    C1    |   (V1)   |    R1    |  R1/C2   |   (V2)   |               
+     *      |  -        2              |          |    C1    |   (V1)   |    R1    |  R1/C2   |   (V2)   |    R2    |          
+     *      |  -        3              |    C1    |   (V1)   |    R1    |  R1/C2   |   (V2)   |    R2    |    R2    |
+     *      |__________________________|__________|__________|__________|__________|__________|__________|__________|
      */
     public boolean isPrimeActiviteAVerser(int numeroMoisSimule, DemandeurEmploi demandeurEmploi) {
 	if (ressourcesFinancieresUtile.hasTravailleAuCoursDerniersMoisAvantSimulation(demandeurEmploi)) {
 	    int nombreMoisTravaillesDerniersMois = periodeTravailleeAvantSimulationUtile.getNombreMoisTravaillesAuCoursDes3DerniersMoisAvantSimulation(demandeurEmploi);
-	    return nombreMoisTravaillesDerniersMois == 1 && numeroMoisSimule == 4 || (nombreMoisTravaillesDerniersMois == 2 && (numeroMoisSimule == 3 || numeroMoisSimule == 6))
-		    || (nombreMoisTravaillesDerniersMois == 3 && (numeroMoisSimule == 2 || numeroMoisSimule == 5));
+	    return nombreMoisTravaillesDerniersMois == 1 && (numeroMoisSimule == 3 || numeroMoisSimule == 6)
+		    || (nombreMoisTravaillesDerniersMois == 2 && (numeroMoisSimule == 2 || numeroMoisSimule == 5))
+		    || (nombreMoisTravaillesDerniersMois == 3 && (numeroMoisSimule == 1 || numeroMoisSimule == 4));
 	}
-	return numeroMoisSimule == 5;
+	return numeroMoisSimule == 4;
     }
 }
--- src/test/java/testsintegration/simulation/temporalite/autresAides/DemandeurTravailleurIndependantTests.java
+++ src/test/java/testsintegration/simulation/temporalite/autresAides/DemandeurTravailleurIndependantTests.java
@@ -133,12 +133,15 @@ class DemandeurTravailleurIndependantTests extends Commun {
 	    assertThat(simulationMensuelle.getRessourcesFinancieres().get(AideEnum.TRAVAILLEUR_INDEPENDANT.getCode())).satisfies(indep -> {
 		assertThat(indep.getMontant()).isEqualTo(100);
 	    });
-	    assertThat(simulationMensuelle.getAides()).hasSize(2);
+	    assertThat(simulationMensuelle.getAides()).hasSize(3);
 	    assertThat(simulationMensuelle.getAides().get(AideEnum.ALLOCATION_SOLIDARITE_SPECIFIQUE.getCode())).satisfies(ass -> {
 		assertThat(ass.getMontant()).isEqualTo(523f);
 	    });
 	    assertThat(simulationMensuelle.getAides().get(AideEnum.ALLOCATION_SOUTIEN_FAMILIAL.getCode())).satisfies(asf -> {
 		assertThat(asf.getMontant()).isEqualTo(117);
+	    });
+	    assertThat(simulationMensuelle.getAides().get(AideEnum.PRIME_ACTIVITE.getCode())).satisfies(ppa -> {
+		assertThat(ppa.getMontant()).isEqualTo(20f);
 	    });
 	});
 	// Alors les prestations du cinquième mois 06/2022 sont :
@@ -153,9 +156,12 @@ class DemandeurTravailleurIndependantTests extends Commun {
 	    assertThat(simulationMensuelle.getRessourcesFinancieres().get(AideEnum.TRAVAILLEUR_INDEPENDANT.getCode())).satisfies(indep -> {
 		assertThat(indep.getMontant()).isEqualTo(100);
 	    });
-	    assertThat(simulationMensuelle.getAides()).hasSize(1);
+	    assertThat(simulationMensuelle.getAides()).hasSize(2);
 	    assertThat(simulationMensuelle.getAides().get(AideEnum.ALLOCATION_SOUTIEN_FAMILIAL.getCode())).satisfies(asf -> {
 		assertThat(asf.getMontant()).isEqualTo(117);
+	    });
+	    assertThat(simulationMensuelle.getAides().get(AideEnum.PRIME_ACTIVITE.getCode())).satisfies(ppa -> {
+		assertThat(ppa.getMontant()).isEqualTo(20f);
 	    });
 	});
 	// Alors les prestations du sixième mois 07/2022 sont :
@@ -170,9 +176,12 @@ class DemandeurTravailleurIndependantTests extends Commun {
 	    assertThat(simulationMensuelle.getRessourcesFinancieres().get(AideEnum.TRAVAILLEUR_INDEPENDANT.getCode())).satisfies(indep -> {
 		assertThat(indep.getMontant()).isEqualTo(100);
 	    });
-	    assertThat(simulationMensuelle.getAides()).hasSize(1);
+	    assertThat(simulationMensuelle.getAides()).hasSize(2);
 	    assertThat(simulationMensuelle.getAides().get(AideEnum.ALLOCATION_SOUTIEN_FAMILIAL.getCode())).satisfies(asf -> {
 		assertThat(asf.getMontant()).isEqualTo(117);
+	    });
+	    assertThat(simulationMensuelle.getAides().get(AideEnum.PRIME_ACTIVITE.getCode())).satisfies(ppa -> {
+		assertThat(ppa.getMontant()).isEqualTo(20f);
 	    });
 	});
     }
